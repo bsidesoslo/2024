829a18fe903f26d4835fb982febedd399124ab26
Erlend Oftedal <erlend@oftedal.no>
2024-10-09 09:16:53 +0200

    Add venue info

--- _convert.sh
+++ _convert.sh
@@ -2,3 +2,7 @@
 # Requires imagemagick
 cd assets/images/speakers
 mogrify -format webp -quality 90 -resize 400x *.png
+cd ../../..
+
+cd assets/images/
+mogrify -format webp -quality 90 -resize 800x *.HEIC
